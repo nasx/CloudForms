893364ff91bccb240489590ed15eb31f2f8588e2
Chris Keller <chris@toast.umbrella.local>
2016-09-21 21:42:27 -0400

    Added idm/idm_list_dns_forward_zones.rb

---
+++ idm/idm_list_dns_forward_zones.rb
@@ -0,0 +1,101 @@
+=begin
+  idm_list_dns_forward_zones.rb
+  Author: Chris Keller <ckeller@redhat.com
+  Description: This method will list DNS Zones managed by IdM
+-------------------------------------------------------------------------------
+   Copyright 2016 Chris Keller <ckeller@redhat.com>
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+-------------------------------------------------------------------------------
+=end
+
+require 'net/http'
+require 'uri'
+require 'json'
+
+begin
+
+username = $evm.object['username']
+hostname = $evm.object['hostname']
+password = $evm.object.decrypt('password')
+
+# First we need to get our ipa_session cookie
+
+url = "https://#{hostname}/ipa"
+uri = URI.parse("#{url}/session/login_password")
+
+request = Net::HTTP::Post.new(uri)
+request["Referer"] = url
+request["Accept"] = "text/plain"
+request.content_type = "application/x-www-form-urlencoded"
+request.set_form_data(
+  "user" => "#{username}",
+  "password" => "#{password}",
+)
+
+response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
+  http.request(request)
+end
+
+if(response.code != '200')
+  $evm.log(:error, "Response Code: #{response.code}")
+  $evm.log(:error, response.to_hash)
+  
+  exit MIQ_ERROR
+end
+
+cookies = response.get_fields('set-cookie')[0].split(";")
+ipa_session = cookies[0].split("=")[1]
+
+# Submit JSON request for dnszone_find
+
+uri = URI.parse("#{url}/session/json")
+
+request = Net::HTTP::Post.new(uri)
+request["Referer"] = url
+request["Accept"] = "application/json"
+request["Cookie"] = "ipa_session=#{ipa_session}"
+request.content_type = "application/json"
+request.body = "{\"method\":\"dnszone_find\",\"params\":[[\"\"],{\"pkey_only\":true,\"sizelimit\":0}]}"
+
+response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
+  http.request(request)
+end
+
+if(response.code != '200')
+  $evm.log(:error, "Response Code: #{response.code}")
+  $evm.log(:error, response.to_hash)
+  
+  exit MIQ_ERROR
+end
+
+json_data = JSON.parse(response.body)
+zone_list = {}
+
+json_data['result']['result'].each do |zone|
+  next if zone['idnsname'][0].include? '.in-addr.arpa.'
+  zone_list[zone['idnsname']] = zone['idnsname']
+end
+
+list_values = {
+  'sort_by' => :value,
+  'required' => false,
+  'default_value' => 'false',
+  'values' => zone_list
+}
+
+list_values.each { |key, value| $evm.object[key] = value }
+
+exit MIQ_OK
+
+end
